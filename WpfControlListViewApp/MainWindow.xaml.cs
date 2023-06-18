a8a9ccfda7baa6fa0fcbacd7b25aa145aaaeed2a
Ефимов Максим <efimov_m@itstep.academy>
2023-06-18 15:40:41 +0300

    Добавьте файлы проекта.

---
+++ WpfControlListViewApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlListViewApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ListBox list = new ListBox();
+            list.Items.Add("Computer");
+            list.Items.Add("Notebook");
+            list.Items.Add("Printer");
+            list.Items.Add("Phone");
+            list.Items.Add("Sacnner");
+            list.HorizontalAlignment = HorizontalAlignment.Left;
+
+            TabItem tab4 = new TabItem();
+            tab4.Header = "Products";
+            tab4.Content = list;
+
+            tabs.Items.Add(tab4);
+        }
+    }
+}
---
+++ WpfControlListViewApp/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlListViewApp
+{
+    internal class Product
+    {
+        public string Title { set; get; }
+        public string Brand { set; get; }
+        public int Price { set; get; }
+    }
+}
